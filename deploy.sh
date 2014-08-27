f8d06d4a932f1fe91852a4cff98d2c1f2326215d
Chris Adams <wave@chrisadams.me.uk>
2014-08-27 11:45:53 +0200

    Update site upload script
    So we don’t needlessly upload identical files each time

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,2 @@
-  # These are deploy lines, reserch them! 
-
-  s3cmd put --recursive source/*  s3://productscience.co.uk
-
-  s3cmd setacl --recursive -P s3://productscience.co.uk/
+# run this command to sync the local version of the site to greenqloud's S3 clone
+s3cmd sync --acl-public --guess-mime-type source/ s3://productscience.co.uk
