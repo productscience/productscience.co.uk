f973c45a2c9d23cfc6651eccbd96ecf179e1df1e
Chris Adams <chris@productscience.co.uk>
2018-12-30 16:25:11 +0000

    Switch out hugo to use eleventy

---
+++ .eleventy.js
@@ -0,0 +1,29 @@
+module.exports = function(eleventyConfig) {
+
+  // register these for copying content across
+  // if we used a pipeline tool, like webpack, we wouldn't use these (I think)
+  eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("img");
+  eleventyConfig.addPassthroughCopy("css");
+
+  return {
+    templateFormats: [
+      "md",
+      "njk",
+    ],
+
+    // If your site lives in a different subdirectory, change this.
+    // Leading or trailing slashes are all normalized away, so don’t worry about it.
+    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+    // This is only used for URLs (it does not affect your file structure)
+    pathPrefix: "/",
+    htmlTemplateEngine: "njk",
+    passthroughFileCopy: true,
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+  };
+};
